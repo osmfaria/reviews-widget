b36727e3ccae1d5d72ebc454c21728c686d78843
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-14 19:47:49 -0800

    feat: include error middleware with app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import errorMiddleware from './middleware/errorMiddleware'
 
 export const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(errorMiddleware)
