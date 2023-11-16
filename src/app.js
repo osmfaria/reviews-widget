7f29548b82d4312a3d9c35e0d07d5007497192c0
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-15 18:13:20 -0800

    refactor: moved app use for errorMiddleware to be the last

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
-import errorMiddleware from './middleware/errorMiddleware'
+import errorMiddleware from './middleware/errorMiddleware.js'
+import appRoutes from './routes/index.js'
 
 export const app = express()
 app.use(cors())
 app.use(express.json())
+appRoutes(app)
+
 app.use(errorMiddleware)
