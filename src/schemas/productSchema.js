9b6faf9200c3f49a1bf62c0479c285f7481c7120
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-17 11:26:15 -0800

    refactor: removed image validation, multer handles it

--- src/schemas/productSchema.js
+++ src/schemas/productSchema.js
@@ -1,25 +1,10 @@
 import * as yup from 'yup'
 
 export const createProductSchema = yup.object().shape({
-  title: yup.string().required(),
+  title: yup.string().required().max(40).min(3),
   price: yup
     .number()
     .positive()
     .required()
     .test((value) => (value.toString().split('.')[1] || '').length <= 2),
-  file: yup
-    .mixed()
-    .required('A file is required')
-    .test(
-      'fileSize',
-      'File too large',
-      (value) => !value || (value && value.size <= 5 * 1024 * 1024)
-    )
-    .test(
-      'fileFormat',
-      'Unsupported Format',
-      (value) =>
-        !value ||
-        (value && ['image/jpeg', 'image/png'].includes(value.mimetype))
-    ),
 })
