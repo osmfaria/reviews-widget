b56647a7d8f3ece1d71d48b0a687e4133df5df56
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-17 11:27:38 -0800

    refactor: add batch upload to s3

--- src/services/createProduct.services.js
+++ src/services/createProduct.services.js
@@ -1,13 +1,13 @@
 import { prisma } from '../database/prisma.js'
-import { v4 as uuid } from 'uuid'
 import { AppError } from '../../errors/appError.js'
 import { S3 } from '@aws-sdk/client-s3'
+import { v4 as uuid } from 'uuid'
 
-const createProductService = async (data, file) => {
-  const fileName = `${uuid()}-${file.originalname}`
+const createProductService = async (data, coverImage, galleryImages) => {
   const bucketName = process.env.BUCKET_NAME
   const region = process.env.REGION
-  const imageUrl = `https://${bucketName}.s3.${region}.amazonaws.com/${fileName}`
+  const coverFileName = `${uuid()}-${coverImage.originalname}`
+  const coverImageUrl = `https://${bucketName}.s3.${region}.amazonaws.com/${coverFileName}`
 
   const s3 = new S3({
     region: region,
@@ -17,22 +17,66 @@ const createProductService = async (data, file) => {
     },
   })
 
-  const params = {
+  const coverImageParams = {
     Bucket: bucketName,
-    Key: fileName,
-    Body: file.buffer,
-    ContentType: file.mimetype,
+    Key: coverFileName,
+    Body: coverImage.buffer,
+    ContentType: coverImage.mimetype,
     ACL: 'public-read',
   }
 
-  try {
-    await s3.putObject(params)
-  } catch (error) {
-    throw new AppError('Failed to upload file', 500)
+  const uploadFilesToS3 = async (file) => {
+    const fileName = `${uuid()}-${file.originalname}`
+    const imageUrl = `https://${bucketName}.s3.${region}.amazonaws.com/${fileName}`
+
+    const batchParams = {
+      Bucket: bucketName,
+      Key: fileName,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+      ACL: 'public-read',
+    }
+    await s3.putObject(batchParams)
+
+    return { image: imageUrl }
   }
 
+  const uploadFilesHandler = async (files) => {
+    if (Array.isArray(files) && files.length) {
+      try {
+        return Promise.all(
+          files.map((file) => {
+            return uploadFilesToS3(file)
+          })
+        )
+      } catch {
+        throw new AppError('Failed to upload files', 400)
+      }
+    }
+  }
+
+  try {
+    await s3.putObject(coverImageParams)
+  } catch {
+    throw new AppError('Failed to upload files', 400)
+  }
+
+  const galleryData = await uploadFilesHandler(galleryImages)
+
   const product = await prisma.product.create({
-    data: { ...data, image: imageUrl },
+    data: {
+      title: data.title,
+      price: Number(data.price),
+      coverImage: coverImageUrl,
+      gallery: {
+        createMany: {
+          data: galleryData,
+        },
+      },
+    },
+    include: {
+      gallery: true,
+    },
   })
 
   return product
