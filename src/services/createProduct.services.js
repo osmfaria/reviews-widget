0df05e3ab35e15ce255ef43ad7249f30f4e3263e
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-16 17:59:33 -0800

    refactor: add aws image url to product object, store on postgresql via prisma

--- src/services/createProduct.services.js
+++ src/services/createProduct.services.js
@@ -1,23 +1,38 @@
 import { prisma } from '../database/prisma.js'
 import { v4 as uuid } from 'uuid'
-import axios from 'axios'
 import { AppError } from '../../errors/appError.js'
+import { S3 } from '@aws-sdk/client-s3'
 
 const createProductService = async (data, file) => {
-  const awsApiUrl = process.env.AWS_API
   const fileName = `${uuid()}-${file.originalname}`
-  const headers = { 'Content-type': file.mimetype }
+  const bucketName = process.env.BUCKET_NAME
+  const region = process.env.REGION
+  const imageUrl = `https://${bucketName}.s3.${region}.amazonaws.com/${fileName}`
+
+  const s3 = new S3({
+    region: region,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
+    },
+  })
+
+  const params = {
+    Bucket: bucketName,
+    Key: fileName,
+    Body: file.buffer,
+    ContentType: file.mimetype,
+    ACL: 'public-read',
+  }
 
   try {
-    await axios.put(`${awsApiUrl}${fileName}`, file.buffer, {
-      headers: headers,
-    })
-  } catch {
-    throw new AppError('failed to upload file', 500)
+    await s3.putObject(params)
+  } catch (error) {
+    throw new AppError('Failed to upload file', 500)
   }
 
   const product = await prisma.product.create({
-    data: { ...data, image: fileName },
+    data: { ...data, image: imageUrl },
   })
 
   return product
