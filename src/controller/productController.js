7572313101d32f184bf1bf4a01067e2e032be4dd
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-17 11:24:54 -0800

    refactor: implement gallery and cover image to be passed to services

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,10 +1,20 @@
 import createProductService from '../services/createProduct.services.js'
+import listProductsService from '../services/listProducts.services.js'
 
 export const createProductController = async (req, res) => {
-  const { file, ...data } = req.validatedData
-  const image = req.file
+  const data = req.body
+  const coverImage = req.files.coverImage ? req.files.coverImage[0] : null
+  const galleryImages = req.files.gallery || []
 
-  const product = await createProductService(data, image)
+  const product = await createProductService(data, coverImage, galleryImages)
 
-  res.status(200).json(product)
+  res.status(201).json(product)
+}
+
+export const listProductsController = async (req, res) => {
+  const name = req.query.name
+
+  const products = await listProductsService(name)
+
+  res.status(200).json(products)
 }
