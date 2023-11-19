af9049ce594fc82086f49c8523e1eb97b4c706d6
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-18 19:31:51 -0800

    feat: included gallery and converImage routes to main route index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
+import coverImageRoutes from './coverImageRoutes.js'
+import galleryRoutes from './galleryRoutes.js'
 import productRouter from './productRoutes.js'
 
 const appRoutes = (app) => {
   app.use('/products', productRouter)
+  app.use('/cover-image', coverImageRoutes)
+  app.use('/gallery', galleryRoutes)
 }
 
 export default appRoutes
