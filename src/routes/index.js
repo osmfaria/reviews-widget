94832928bf45eb58c1300b3aec258418220279ae
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-22 14:30:45 -0800

    feat: implement route for docs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,21 @@ import coverImageRoutes from './coverImageRoutes.js'
 import galleryRoutes from './galleryRoutes.js'
 import productRouter from './productRoutes.js'
 import reviewRoutes from './reviewRoutes.js'
+import swaggerUi from 'swagger-ui-express'
+import fs from 'fs'
+
+const loadJSON = (path) =>
+  JSON.parse(fs.readFileSync(new URL(path, import.meta.url)))
+
+const swaggerDocs = loadJSON('../../swagger.json')
+swaggerDocs.host = process.env.API_URL || 'localhost:5000'
 
 const appRoutes = (app) => {
   app.use('/products', productRouter)
   app.use('/cover-image', coverImageRoutes)
   app.use('/gallery', galleryRoutes)
   app.use('/reviews', reviewRoutes)
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 }
 
 export default appRoutes
